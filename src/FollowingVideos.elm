3ac0f19558fc3c3d986b2a4d5d232cbfe256f923
Justin Love <git@JustinLove.name>
2018-03-03 11:30:56 -0600

    extract helix function to twitch module

--- src/FollowingVideos.elm
+++ src/FollowingVideos.elm
@@ -4,6 +4,7 @@ import Persist exposing (Persist)
 import Persist.Encode
 import Persist.Decode
 import Twitch.Deserialize exposing (User, Follow, Video)
+import Twitch exposing (helix)
 import TwitchId
 import View
 import Harbor
@@ -296,38 +297,6 @@ fetchVideos auth userId =
     , tagger = Videos
     , url = (fetchVideosUrl userId)
     }
-
-helix :
-  { clientId : String
-  , auth : Maybe String
-  , decoder : Json.Decode.Decoder a
-  , tagger : ((Result Http.Error a) -> Msg)
-  , url : String
-  } -> Cmd Msg
-helix {clientId, auth, decoder, tagger, url} =
-  Http.send tagger <| Http.request
-    { method = "GET"
-    , headers = twitchHeaders clientId auth
-    , url = url
-    , body = Http.emptyBody
-    , expect = Http.expectJson decoder
-    , timeout = Nothing
-    , withCredentials = False
-    }
-
-twitchHeaders : String -> Maybe String -> List Http.Header
-twitchHeaders clientId auth =
-  List.append
-    [ Http.header "Client-ID" clientId
-    ] (authHeaders auth)
-
-authHeaders : Maybe String -> List Http.Header
-authHeaders auth =
-  case auth of
-    Just token -> 
-      [ Http.header "Authorization" ("Bearer "++token) ]
-    Nothing ->
-      []
 
 extractHashArgument : String -> Location -> Maybe String
 extractHashArgument key location =
